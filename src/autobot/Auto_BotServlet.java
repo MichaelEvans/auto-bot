1499f504c3d8d27aca5cd7ff8cc46e19d0d4b9ef
bmwracer0 <bmwracer0@feece9b8-deee-11de-a3a5-2133a16dfa60>
2009-12-02 17:59:19 +0000

    added weather

--- src/autobot/Auto_BotServlet.java
+++ src/autobot/Auto_BotServlet.java
@@ -1,7 +1,7 @@
 /* 
   Copyright (c) 2009 Michael Evans
   All rights reserved.
- 
+
   Redistribution and use in source and binary forms, with or without
   modification, are permitted provided that the following conditions
   are met:
@@ -10,7 +10,7 @@
   2. Redistributions in binary form must reproduce the above copyright
      notice, this list of conditions and the following disclaimer in the
      documentation and/or other materials provided with the distribution.
- 
+
   THIS SOFTWARE IS PROVIDED BY THE AUTHOR(S) ``AS IS'' AND ANY EXPRESS OR
   IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
   OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
@@ -23,32 +23,43 @@
   THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package autobot;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 import java.util.logging.Logger;
+import java.util.regex.*;
+import java.util.Random;
 
 import com.google.wave.api.*;
 
 public class Auto_BotServlet extends AbstractRobotServlet {
 	static final Logger log = Logger.getLogger(Auto_BotServlet.class.getName()); 
-	public String COMMAND = "!@force_new_wave@!";
+	public String COMMAND = "!@force-new-wave@!";
 	public int MAX_BLIPS = 150;
 	public HashMap<String, Integer> votes = new HashMap<String, Integer>();
+	int NUM_OF_VOTES = votes.size();
+	public ArrayList<String> activeWavers = new ArrayList<String>();
+	int ACTIVE_WAVERS = 0;
+	Image optimusTransform = new Image("http://imgur.com/m66zH.gif", 160, 120, "");
+	Random generator = new Random();
+	String weatherURL = "http://weather.yahooapis.com/forecastrss?p=";
+		
 	public void processEvents(RobotMessageBundle bundle) {
 		Wavelet wavelet = bundle.getWavelet();
 		if (bundle.wasSelfAdded()) {
 			Blip blip = wavelet.appendBlip();
 			TextView textView = blip.getDocument();
 			textView.append("Autobots roll out.");
+			blip.getDocument().appendElement(optimusTransform);
 		}
+		int NUM_OF_PARTICIPANTS = wavelet.getParticipants().size();
+
 		for (Event e: bundle.getEvents()) {
 			if (e.getType() == EventType.WAVELET_PARTICIPANTS_CHANGED) {    
-				//Blip blip = wavelet.appendBlip();
-				//TextView textView = blip.getDocument();
-				//textView.append("Participant Added.");
 			}
+
 			if (e.getType() == EventType.BLIP_SUBMITTED){
 				Blip root = wavelet.getRootBlip();
 				List<String> childrenIds = root.getChildBlipIds(); 
@@ -57,23 +68,14 @@ public class Auto_BotServlet extends AbstractRobotServlet {
 					children.add(bundle.getBlip(wavelet.getWaveId(), wavelet.getWaveletId(), id)); 
 				}*/
 				processBlip(e.getBlip(), wavelet);
-				if(childrenIds.size()==MAX_BLIPS){
+				if(childrenIds.size()==MAX_BLIPS+NUM_OF_VOTES){
 					Wavelet newWave = wavelet.createWavelet(wavelet.getParticipants(), "ID");
 					String title = getNewTitle(wavelet);
 					newWave.setTitle(title);
 				}
-				
+
 			}
 			/*if (e.getType() == EventType.BLIP_DELETED){
-				Blip blip = wavelet.appendBlip();
-				TextView textView = blip.getDocument();
-				textView.append("Blip Deleted");
-				Wavelet newWave = wavelet.createWavelet(wavelet.getParticipants(), "ID");
-				Blip newBlip = newWave.appendBlip();
-				TextView text = newBlip.getDocument();
-				text.append("New Wave Created");
-				//log.warning("Title Changed");
-				//Wavelet newWave = wavelet.createWavelet(wavelet.getParticipants(), "DataDocument");
 			}*/
 		}
 	}
@@ -83,6 +85,10 @@ public class Auto_BotServlet extends AbstractRobotServlet {
 		String text = blip.getDocument().getText();
 		String author = wavelet.getCreator();
 		String authorRequest = blip.getCreator();
+		if(!activeWavers.contains(authorRequest)){
+			ACTIVE_WAVERS++;
+			activeWavers.add(authorRequest);
+		}
 		log.info("Wave Creator: "+ author + "Blip from: " + authorRequest+"\n");
 		if (text.startsWith(COMMAND) && author.equals(authorRequest)) {
 			log.info("Forced a new wave.");
@@ -90,34 +96,104 @@ public class Auto_BotServlet extends AbstractRobotServlet {
 			String title = getNewTitle(wavelet);
 			newWave.setTitle(title);
 		}
-		if(text.startsWith("!@add_vote@!")){
-			//Gadget voter = new Gadget("http://wave-poll.googlecode.com/svn/trunk/src/poll.xml");
+		if(text.startsWith("!@roll-out@!")){
 			String voteCreator = blip.getCreator();
+			blip.getDocument().append("\nFreedom is your right. If you make that request we will honor it.");
 			votes.put(voteCreator, 1);
 			int i = 0;
 			Set<String> users = votes.keySet();
-			for(String user:users){
+			/*for(String user:users){
 				if(votes.get(user)==1)
 					i++;
-			}
+			}*/
+			NUM_OF_VOTES = votes.size();
 			String rootText = wavelet.getRootBlip().getDocument().getText();
-			//String newText = rootText.substring(0,rootText.indexOf("Number of votes for new wave: "));
-			//wavelet.getRootBlip().getDocument().delete();
-			wavelet.getRootBlip().getDocument().append("Number of votes for new wave: "+i);
+			int index = rootText.indexOf("Wave Max: ");
+			if(index<0){
+				String appendText = "\n\n" + "Wave Max: " + (NUM_OF_VOTES + MAX_BLIPS) + "\nNumber of votes for new wave: "+NUM_OF_VOTES;
+				//wavelet.getRootBlip().getDocument().delete();
+				wavelet.getRootBlip().getDocument().append(appendText);
+			}else{
+				String newText = rootText.substring(0,index);
+				wavelet.getRootBlip().getDocument().delete();
+				wavelet.getRootBlip().getDocument().append(newText + "Wave Max: " + (NUM_OF_VOTES + MAX_BLIPS) + "\nNumber of votes for new wave: "+NUM_OF_VOTES);
+			}
+			if(NUM_OF_VOTES>((1/3)*ACTIVE_WAVERS)&&(ACTIVE_WAVERS>=4))
+				makeNewWave(wavelet);
 		}
+		/*if(text.startsWith("!@russian-roulette@!")){
+			int drop = generator.nextInt(wavelet.getParticipants().size());
+			//Blip newBlip = wavelet.appendBlip();
+			TextView textView = blip.getDocument();
+			textView.append("\nThanks for transforming " + wavelet.getParticipants().get(drop) + ".");
+			wavelet.removeParticipant(wavelet.getParticipants().get(drop));
+		}*/
+		//
+		if(text.startsWith("!@weather")){
+			Pattern banP = Pattern.compile("!@weather:(\\d{5})@!");
+			Matcher mtchr = banP.matcher(text);
+			mtchr.lookingAt();
+			try{
+				//ban
+				String current = "";
+				String image = "";
+				try {
+					current = XMLParser.getLocation(Integer.parseInt(mtchr.group(1)));
+					current += "\nNow - " + XMLParser.getTemp(Integer.parseInt(mtchr.group(1)));
+					current += "\n" + XMLParser.getForecast(Integer.parseInt(mtchr.group(1)));
+					image = XMLParser.getImage(Integer.parseInt(mtchr.group(1)));
+				} catch (NumberFormatException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				blip.getDocument().replace(current);
+				blip.getDocument().appendElement(new Image(image, 52,52,""));
+			}catch (IllegalStateException e){
+				//blip.getDocument().replace("Degrees");
+			}catch (IndexOutOfBoundsException e){
+				//not this many matches
+			}
+		}
+		if(text.startsWith("!@vote-to-ban:")){
+			Pattern banP = Pattern.compile("!@vote-to-ban:(.+)@!");
+			Matcher mtchr = banP.matcher(text);
+			mtchr.lookingAt();
+			try{
+				//ban
+				wavelet.getRootBlip().getDocument().append("\n" + blip.getCreator() + " motions to ban " + mtchr.group(1) + ".");
+			}catch (IllegalStateException e){
+				wavelet.getRootBlip().getDocument().append("\n" + blip.getCreator() + " loses their ban vote privileges.");
+			}catch (IndexOutOfBoundsException e){
+				//not this many matches
+			}
+			//wavelet.removeParticipant(mtchr.group(1));
+		}
+	}
+
+	private void makeNewWave(Wavelet wavelet) {
+		String title = getNewTitle(wavelet);
+		Wavelet newWave = wavelet.createWavelet(wavelet.getParticipants(), "ID");
+		newWave.setTitle(title);
 	}
 
 	private String getNewTitle(Wavelet wavelet) {
 		// TODO Auto-generated method stub
 		String Title = wavelet.getTitle();
+		/*int indexMax = wavelet.getRootBlip().getDocument().getText().indexOf("Wave Max: ");
+		if(indexMax>-1){
+			Title = wavelet.getRootBlip().getDocument().getText().substring(0,indexMax);
+		}*/
 		int index = Title.indexOf("//Part");
 		if(index == -1){
-	    	Title = Title + " //Part 2";
-	    }else{
-	    	int count = Integer.parseInt(Title.substring(index+6).trim());
-	    	count++;
-	    	Title = Title.substring(0,index) + " //Part " + count;
-	    }
+			Title = Title + " //Part 2";
+		}else{
+			int count = Integer.parseInt(Title.substring(index+6).trim());
+			count++;
+			Title = Title.substring(0,index) + " //Part " + count;
+		}
 		return Title;
 	}
 
