c118a4cc698b3617794222549d4521cf1303a6c6
n.lefler <n.lefler@feece9b8-deee-11de-a3a5-2133a16dfa60>
2010-03-17 01:29:13 +0000

    Added gadget xml

--- src/frontend/IndexServlet.java
+++ src/frontend/IndexServlet.java
@@ -1,7 +1,14 @@
 package frontend;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class IndexServlet extends HttpServlet {
-
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+	throws IOException {
+		;
+	}
 }
