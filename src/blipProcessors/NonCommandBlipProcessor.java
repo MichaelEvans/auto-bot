b21312ddd8ac8a554f726a78d3a518353ce829d6
n.lefler <n.lefler@feece9b8-deee-11de-a3a5-2133a16dfa60>
2010-03-25 02:30:34 +0000

    Fixed logging

--- src/blipProcessors/NonCommandBlipProcessor.java
+++ src/blipProcessors/NonCommandBlipProcessor.java
@@ -2,6 +2,7 @@ package blipProcessors;
 
 import java.util.Map;
 import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,25 +12,26 @@ import com.google.wave.api.Blip;
 import com.google.wave.api.Wavelet;
 
 public class NonCommandBlipProcessor implements IBlipProcessor {
+	private static Logger log = Logger.getLogger(NonCommandBlipProcessor.class.getName());
 
 	public Wavelet processBlip(Blip blip, Wavelet wavelet,
 			Map<String, Object> dataMap) {
 
-                String cont = blip.getContent().trim().toLowerCase();
-                if (cont.matches("^*this\\.*")) {
-                        autobot.Auto_BotServlet.log.log(Level.INFO, "Post is "
-                        + "useless " + cont);
-                        blip.all().delete();
-                        Utils.appendToBlip(blip, "This post was completely "
-                        + "useless.  Instead, have a picture\n!{::random}!");
-                        return (wavelet);
-                }
+		String cont = blip.getContent().trim().toLowerCase();
+		if (cont.matches("^*this\\.*")) {
+			log.log(Level.INFO, "Post is "
+					+ "useless " + cont);
+			blip.all().delete();
+			Utils.appendToBlip(blip, "This post was completely "
+					+ "useless.  Instead, have a picture\n!{::random}!");
+			return (wavelet);
+		}
 
 		Pattern chanQuote = Pattern.compile("(\\^|\\n|\\A){1}>.*");
 		Matcher mtchr = chanQuote.matcher(blip.getContent());
-		
+
 		while(mtchr.find()) {
-			autobot.Auto_BotServlet.log.log(Level.INFO, "Found something: " + mtchr.group());
+			log.log(Level.INFO, "Found something: " + mtchr.group());
 			blip.all().delete();
 			Utils.appendToBlip(blip, "This isn't 4chan.");
 			blip.all().annotate("style/fontWeight", "bold");
